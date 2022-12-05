3a7961303a70523665474a2aabf6000fb360afe8
Lsig <logis21@ru.is>
2022-12-05 20:04:36 +0000

    hmm

--- lua/lsig/treesitter.lua
+++ lua/lsig/treesitter.lua
@@ -1,9 +1,8 @@
-local treesitter = require "nvim-treesitter.configs"
-
-treesitter.setup {
-    ensure_installed = {"lua", "typescript", "python", "javascript"},
-    sync_install = false,
-    highlight = {enable = true, additional_vim_regex_highlighting = false},
-    indent = {enable = true},
-    autotag = {enable = true}
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {"c", "lua", "rust", "python", "typescript", "javascript"},
+  ensure_installed = {"lua", "typescript", "python", "javascript"},
+  sync_install = false,
+  highlight = {enable = true, additional_vim_regex_highlighting = false},
+  indent = {enable = true},
+  autotag = {enable = true},
 }
