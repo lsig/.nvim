142d1277284d12c3c1be06d14efe7f2af6de1acf
Lsig <logis21@ru.is>
2022-12-17 17:36:27 +0000

    made config more readable and orginezed, added new cmp remap

--- lua/lsig/remap.lua
+++ lua/lsig/remap.lua
@@ -33,6 +33,34 @@ nnoremap("<leader>y", '"+y')
 nnoremap("<leader>Y", '"+Y')
 nnoremap("Q", "<nop>")
 
+-- lspsaga
+-- show definition, references
+nnoremap("gf", "<silent>:Lspsaga lsp_finder<CR>")
+-- got to declaration
+nnoremap("gD", "<cmd>:lua vim.lsp.buf.declaration()<CR>")
+-- see definition and make edits in window
+nnoremap("gd", "<silent>:Lspsaga peek_definition<CR>")
+-- go to implementation
+nnoremap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+-- see available code actions
+nnoremap("<leader>ca", "<silent>:Lspsaga code_action<CR>")
+-- smart rename
+nnoremap("<leader>rn", "<silent>:Lspsaga rename<CR>")
+-- show  diagnostics for line
+nnoremap("<leader>D", "<silent>:Lspsaga show_line_diagnostics<CR>")
+-- show diagnostics for cursor
+nnoremap("<leader>d", "<silent>:Lspsaga show_cursor_diagnostics<CR>")
+-- jump to previous diagnostic in buffer
+nnoremap("[d", "<silent>:Lspsaga diagnostic_jump_prev<CR>")
+-- jump to next diagnostic in buffer
+nnoremap("]d", "<silent>:Lspsaga diagnostic_jump_next<CR>")
+-- show documentation for what is under cursor
+nnoremap("K", "<silent>:Lspsaga hover_doc<CR>")
+-- see outline on right hand side
+nnoremap("<leader>o", "<cmd>LSoutlineToggle<CR>")
+-- Float terminal
+nnoremap("<A-d>", "<silent>:Lspsaga open_floatterm")
+
 -- insert mode remaps
 inoremap("jk", "<ESC>")
 
