d151acbdda025612856614132c62f5d367753103
Lsig <logis21@ru.is>
2022-12-31 01:29:28 +0000

    made prettier

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,8 +1,7 @@
-
 -- empty setup using defaults
 require("nvim-tree").setup({
-    sync_root_with_cwd = true,
-    respect_buf_cwd = true,
-    update_focused_file = {enable = true, update_root = true},
-    git = {ignore = false}
+	sync_root_with_cwd = true,
+	respect_buf_cwd = true,
+	update_focused_file = { enable = true, update_root = true },
+	git = { ignore = false },
 })
