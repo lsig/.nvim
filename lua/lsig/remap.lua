2017732263bb02b848a8cb103e4280fa5074ddc5
Lsig <logis21@ru.is>
2023-04-07 13:43:48 +0000

    Added git files telescope remap leader gf

--- lua/lsig/remap.lua
+++ lua/lsig/remap.lua
@@ -5,6 +5,7 @@ local xnoremap = require("lsig.keymap").xnoremap
 
 -- normal mode remaps
 nnoremap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
+nnoremap("<leader>gf", "<cmd>lua require('telescope.builtin').git_files()<CR>")
 nnoremap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
 nnoremap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>")
 nnoremap("<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>")
