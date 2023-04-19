aedccc1b048cc94865bceb2e9b565f73cc5191d1
Lsig <logis21@ru.is>
2023-04-19 21:06:51 +0000

    cleaned up cmp

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -75,28 +75,3 @@ cmp.setup.cmdline(":", {
 		{ name = "cmdline" },
 	}),
 })
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-require("lspconfig")["pyright"].setup({
-	capabilities = capabilities,
-})
-require("lspconfig")["rust_analyzer"].setup({
-	capabilities = capabilities,
-})
-require("lspconfig")["clangd"].setup({
-	capabilities = capabilities,
-})
-require("lspconfig")["html"].setup({
-	capabilities = capabilities,
-})
-require("lspconfig")["cssls"].setup({
-	capabilities = capabilities,
-})
-require("lspconfig")["tsserver"].setup({
-	capabilities = capabilities,
-})
-require("lspconfig")["lua_ls"].setup({
-	capabilities = capabilities,
-})
