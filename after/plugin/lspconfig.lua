5d7065662aa9e9e3ee60e0ca0f024287550fa5c4
Lsig <logis21@ru.is>
2023-07-18 11:42:17 +0000

    Added on buffer attach remaps into lsp and added gopls

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,21 +1,49 @@
 require("neodev").setup()
 require("mason").setup()
-require("lsig.keymap")
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+local on_attach = function(_, bufnr)
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
+
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
+
+	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	nmap("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+	-- See `:help K` for why this keymap
+	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+	-- Lesser used LSP functionality
+	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+	nmap("<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "[W]orkspace [L]ist Folders")
+
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, { desc = "Format current buffer with LSP" })
+end
+
 local servers = {
-	-- pylyzer = {
-	-- 	python = {
-	-- 		checkOnType = true,
-	-- 		diagnostics = false,
-	-- 		inlayHints = true,
-	-- 		smartCompletion = true,
-	-- 	},
-	-- },
 	pyright = {
 		python = {
 			analysis = {
@@ -33,6 +61,13 @@ local servers = {
 	clangd = {},
 	sqlls = {},
 	tailwindcss = {},
+	gopls = {
+		completeUnimported = true,
+		usePlaceholders = true,
+		analysis = {
+			unusedparams = true,
+		},
+	},
 	rust_analyzer = {
 		checkOnSave = {
 			command = "clippy",
@@ -96,6 +131,7 @@ mason_lspconfig.setup_handlers({
 	function(server_name)
 		require("lspconfig")[server_name].setup({
 			capabilities = capabilities,
+			on_attach = on_attach,
 			settings = servers[server_name],
 		})
 	end,
@@ -110,7 +146,6 @@ mason_null_ls.setup({
 		"eslint_d",
 		"ruff",
 		"sql_formatter",
-		"rustfmt",
 		"clangd_format",
 		"mypy",
 		"cpplint",
--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -20,6 +20,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.sql_formatter,
 		null_ls.builtins.formatting.ruff,
+		null_ls.builtins.formatting.gofmt,
 		null_ls.builtins.completion.luasnip,
 		null_ls.builtins.diagnostics.ruff,
 		null_ls.builtins.diagnostics.mypy,
